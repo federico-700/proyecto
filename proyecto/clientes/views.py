8efc6ec050e9af3184297ad6f6b1300864ee2aa0
Federico Domañski <fededoma7@gmail.com>
2024-08-22 21:18:01 -0300

    listado de paises, funciona, clase 19

--- proyecto/clientes/views.py
+++ proyecto/clientes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Pais, Cliente
 
-# Create your views here.
+#def index(request):
+#   return render(request, 'clientes/index.html')
+
+def pais_list(request):
+    paises = Pais.objects.all()
+    contexto = {'paises': paises}
+    return render(request, 'clientes/pais_list.html', contexto)
+
+
+#def cliente_list(request):
+#    clientes = Cliente.objects.all()
+#    contexto = {'clientes': clientes}
+#    return render(request, 'clientes/cliente_list.html', contexto)
+
+
+
